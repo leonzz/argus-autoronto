3dbfdeadc2113e718602b702bfcef6042e676e1d
jsp20 <47089025+jsp20@users.noreply.github.com>
2019-03-30 21:32:37 -0700

    Add files via upload
    Changed index.js file to show odometry, imu, planned path, car information and obstacle information. 
    
    Changed xviz-server to show object information and started one way of showing the planned path.

--- rosbridge-xviz-connector/index.js
+++ rosbridge-xviz-connector/index.js
@@ -1,8 +1,31 @@
 const ROSLIB = require("roslib");
 const xvizServer = require('./xviz-server');
 
-// These variables represent the x,y,z values of the vector spline for the car path, Lg is the length of the vector and i is the index 
-var X= [],Y = [], Z = [], Lg, i,Vertex=[];
+/* 
+The purpose of these x,y,z is to send what the ros topic considers to be a pose x,y,z in UTM coordinates and transform them into a set of lattitude and longitude
+values which we can use to set an object in the map. */
+var X = [0], Y = [0], Z = [0],i=0, k =[], Vertex = [] ;
+
+
+//these variables set up the projected car path of the car 
+var CarPath = [];
+
+// these variables will be used to store the map infromation for objects in the car FOV. 
+var obs_lat = [0], obs_long = [0], Found_object = 0, obs_info = [0]; obs_UTM = [0];
+var delta_object = [];
+
+//these variables will be used to store the map information for the car 
+var car_lat = [0]; car_lon = [0], car_alt = [0];
+var car_info = [0]; delta_car = [], car_UTM= [] ;
+var car_x0 = [0], car_y0 = [0], car_z0 = [0], car_x = [], car_y = [], car_z = [],car_odom = [];
+
+//Value used to store IMU heading value 
+var W = [0], Orient_x = [], Orient_y = [], Orient_y = [], Orient_w = [];
+
+
+/***************************************************************
+//section of the code where we subscribe to the multiple topics published by the rosbag
+*****************************************************************/
 
 const rosBridgeClient = new ROSLIB.Ros({
     url : 'ws://localhost:9090'
@@ -19,6 +42,32 @@ const listener2 = new ROSLIB.Topic({
     ros : rosBridgeClient,
     name : '/PathPlanner/desired_path'
 });
+
+
+
+// for obstacle information
+const listener3 = new ROSLIB.Topic({
+    ros : rosBridgeClient,
+    name : '/planner_obstacles'
+});
+
+
+// for obstacle information
+const listener4 = new ROSLIB.Topic({
+    ros : rosBridgeClient,
+    name : '/navsat/odom'
+});
+
+
+// for obstacle information
+const listener5 = new ROSLIB.Topic({
+    ros : rosBridgeClient,
+    name : '/imu/data'
+});
+
+/***********************************************************/
+
+
 
 
 xvizServer.startListenOn(8081);
@@ -51,29 +100,223 @@ rosBridgeClient.on('close', function() {
 listener.subscribe(function(message) {
     // var msgNew = 'Received message on ' + listener.name + JSON.stringify(message, null, 2) + "\n";
     let timestamp = `${message.header.stamp.secs}.${message.header.stamp.nsecs}`;
-    xvizServer.updateLocation(message.header.seq, message.latitude, message.longitude, message.altitude, parseFloat(timestamp));
+
+
+
+    // combine car info from rosbag into a single property to be piped into the xviz-server
+    car_lat = message.latitude;
+    car_lon = message.longitude;
+    car_alt = message.altitude;
+
+    //ful list of car info in format latitude, longitude, altitude
+    car_info = [ car_lat, car_lon, car_alt, W];
+    xvizServer.updateLocation(message.header.seq, car_info, obs_info, car_odom, CarPath, parseFloat(timestamp));
+});
+
+//listener 2 which is used to pipe the road information
+listener2.subscribe(function (message) {
+
+
+    Lg = message.poses.length;
+
+//Grab the first instance of position. The projected path will be a delta of UTM positions or where the car will be in space relative to the position at time= t1
+
+    X0 = message.poses[0].pose.position.x;
+    Y0 = message.poses[0].pose.position.y;
+    Z0 = message.poses[0].pose.position.z;
+
+    var X_path = new Float64Array(Lg); 
+    var Y_path = new Float64Array(Lg);
+    var Z_path = new Float64Array(Lg);
+   //This function can be pulled out of here but I have not had time to do this. 
+        for (var j = 1; j <= Lg-1; j++) {
+
+            X_path[j-1] = message.poses[j].pose.position.x-X0;
+            Y_path[j-1] = message.poses[j].pose.position.y-Y0;
+            Z_path[j-1] = message.poses[j].pose.position.z-Z0;
+
+            CarPath[j - 1] = [X_path[j - 1], Y_path[j - 1], Z_path[j - 1]];
+        }
+
+
+});
+
+//listener 3 is the information of an object in the path of the car 
+listener3.subscribe(function (message) {
+    // let timestamp = `${message.markers.header.stamp.secs}.${message.markers.header.stamp.nsecs}`;
+
+    X = message.markers[0].pose.position.x;
+    Y = message.markers[0].pose.position.y;
+    Z = message.markers[0].pose.position.z;
+
+    obs_UTM = [X, Y, Z];
+
+    if (X != 0 && Y != 0) {
+        //object found! 
+        Found_object = 1;
+    }
+
+
+
+    obs_info = [delta_object[0], delta_object[1], delta_object[2], Found_object];
+
 });
 
 
-//listener 2 which is used to pipe the road information
-/* listener2.subscribe(function(message) {
-    let timestamp = `${message.header.stamp.secs}.${message.header.stamp.nsecs}`;
-
-    Lg =message.poses.length  
-    // we will setup a for loop to find the value of x at every instance in the ros bag 
-
-    for( i = 0; i < Lg-1; i++){
-    X[i]= message.poses[i].pose.position.x;
-    Y[i]= message.poses[i].pose.position.y;
-    Z[i]= message.poses[i].pose.position.z;
+//listener 4 is the odometry information of the car 
+listener4.subscribe(function (message) {
+    // let timestamp = `${message.markers.header.stamp.secs}.${message.markers.header.stamp.nsecs}`;
 
 
-    Vertex[i] = [X[i], Y[i], Z[i] ];
-    }
-    xvizServer.updateCarPath(message.header.seq,Vertex);
-}); */
+    if (car_x0 == 0, car_y0 == 0, car_z0 == 0) {
+
+        car_x0 = message.pose.pose.position.x;
+        car_y0 = message.pose.pose.position.y;
+        car_z0 = message.pose.pose.position.z;
+           }
+
+// we remove the initial position to get the relative motion of the car from time t0 
+    car_x = message.pose.pose.position.x - car_x0;
+    car_y = message.pose.pose.position.y - car_y0;
+    car_z = message.pose.pose.position.z - car_z0;
+
+    car_odom = [car_x, car_y, car_z];
 
 
+
+
+    car_UTM = [message.pose.pose.position.x,message.pose.pose.position.y,message.pose.pose.position.z];
+
+
+     delta_object = distance(obs_UTM, car_UTM);
+
+});
+
+
+//listener 5 is the imu data for the car 
+listener5.subscribe(function (message) {
+        
+   // W = Math.atan(message.orientation.x, message.orientation.y)*Math.PI/180;
+    Orient_x = message.orientation.x;
+    Orient_y = message.orientation.y;
+    Orient_z = message.orientation.z;
+    Orient_w = message.orientation.w;
+
+    W = Orient_w;
+
+     
+
+});
+
+//function desgined to convert from UTM to long,lat 
+function GetLatLon(easting, northing) {
+    /*for the purposes of this function we assume that values related to X are the eastings 
+    for the purposes of this function we assume that values related to Y are the northing
+    
+The main purpose of this function is to conver the UTM coordinates to latitude and longitude for an arbritary set of given coordinates. 
+Assumed:
+- Zone = 17 (Which is the value for Toronto. 
+
+Code is adapted from: https://www.movable-type.co.uk/scripts/latlong-utm-mgrs.html?fbclid=IwAR3LFB4iyQzEqcrZEynbiDZWb1gbTh-t4z6FeIIweW_0c0zLrQ8otX6q-Pk 
+    */
+
+    //This is modified from the following website: https://www.movable-type.co.uk/scripts/latlong-utm-mgrs.html?fbclid=IwAR3YtlscFlgl6K4pral7wCJzlIVeuEx3BPSSmWg6pxJ1-URjSBoRqorK6vo
+
+
+    // This value will need to be set by the user prior to starting the model. This is in the case the car is operating somewere else other than Toronto which is zone 17  
+    const z = 17;
+    // This value will need to be set by the user prior to starting the model. This is in the case the car is operating in the southern hemisphere 
+    const h = 'N';
+
+    const falseEasting = 500e3, falseNorthing = 10000e3;
+
+    const a = 6378137, f = 1 / 298.257223563;
+    // WGS-84: a = 6378137, b = 6356752.314245, f = 1/298.257223563;
+
+    const k0 = 0.9996; // UTM scale on the central meridian
+
+    const x = easting - falseEasting;                            // make x ± relative to central meridian
+    const y = h == 'S' ? northing - falseNorthing : northing; // make y ± relative to equator
+
+
+    // ---- from Karney 2011 Eq 15-22, 36:
+
+    const e = Math.sqrt(f * (2 - f)); // eccentricity
+    const n = f / (2 - f);        // 3rd flattening
+    const n2 = n * n, n3 = n * n2, n4 = n * n3, n5 = n * n4, n6 = n * n5;
+
+    const A = a / (1 + n) * (1 + 1 / 4 * n2 + 1 / 64 * n4 + 1 / 256 * n6); // 2πA is the circumference of a meridian
+
+    const η = x / (k0 * A);
+    const ξ = y / (k0 * A);
+
+    const β = [null, // note β is one-based array (6th order Krüger expressions)
+        1 / 2 * n - 2 / 3 * n2 + 37 / 96 * n3 - 1 / 360 * n4 - 81 / 512 * n5 + 96199 / 604800 * n6,
+               1 / 48 * n2 + 1 / 15 * n3 - 437 / 1440 * n4 + 46 / 105 * n5 - 1118711 / 3870720 * n6,
+                        17 / 480 * n3 - 37 / 840 * n4 - 209 / 4480 * n5 + 5569 / 90720 * n6,
+                                 4397 / 161280 * n4 - 11 / 504 * n5 - 830251 / 7257600 * n6,
+                                               4583 / 161280 * n5 - 108847 / 3991680 * n6,
+                                                             20648693 / 638668800 * n6];
+
+    let ξʹ = ξ;
+    for (let j = 1; j <= 6; j++) ξʹ -= β[j] * Math.sin(2 * j * ξ) * Math.cosh(2 * j * η);
+    
+
+    let ηʹ = η;
+    for (let j = 1; j <= 6; j++) ηʹ -= β[j] * Math.cos(2 * j * ξ) * Math.sinh(2 * j * η);
+
+    const sinhηʹ = Math.sinh(ηʹ);
+    const sinξʹ = Math.sin(ξʹ), cosξʹ = Math.cos(ξʹ);
+
+    const τʹ = sinξʹ / Math.sqrt(sinhηʹ * sinhηʹ + cosξʹ * cosξʹ);
+
+    let δτi = null;
+    let τi = τʹ;
+    do {
+        const σi = Math.sinh(e * Math.atanh(e * τi / Math.sqrt(1 + τi * τi)));
+        const τiʹ = τi * Math.sqrt(1 + σi * σi) - σi * Math.sqrt(1 + τi * τi);
+        δτi = (τʹ - τiʹ) / Math.sqrt(1 + τiʹ * τiʹ)
+            * (1 + (1 - e * e) * τi * τi) / ((1 - e * e) * Math.sqrt(1 + τi * τi));
+        τi += δτi;
+    } while (Math.abs(δτi) > 1e-12); // using IEEE 754 δτi -> 0 after 2-3 iterations
+    // note relatively large convergence test as δτi toggles on ±1.12e-16 for eg 31 N 400000 5000000
+    const τ = τi;
+
+    const φ = Math.atan(τ);
+    let λ = Math.atan2(sinhηʹ, cosξʹ);
+
+    // ---- convergence: Karney 2011 Eq 26, 27
+
+    let p = 1;
+    for (let j = 1; j <= 6; j++) p -= 2 * j * β[j] * Math.cos(2 * j * ξ) * Math.cosh(2 * j * η);
+    let q = 0;
+    for (let j = 1; j <= 6; j++) q += 2 * j * β[j] * Math.sin(2 * j * ξ) * Math.sinh(2 * j * η);
+
+    const γʹ = Math.atan(Math.tan(ξʹ) * Math.tanh(ηʹ));
+    const γʺ = Math.atan2(q, p);
+
+    const γ = γʹ + γʺ;
+
+    // ---- scale: Karney 2011 Eq 28
+
+    const sinφ = Math.sin(φ);
+    const kʹ = Math.sqrt(1 - e * e * sinφ * sinφ) * Math.sqrt(1 + τ * τ) * Math.sqrt(sinhηʹ * sinhηʹ + cosξʹ * cosξʹ);
+    const kʺ = A / a / Math.sqrt(p * p + q * q);
+
+    const k = k0 * kʹ * kʺ;
+
+    // ------------
+
+    const λ0 = ((z - 1) * 6 - 180 + 3)*Math.PI/180; // longitude of central meridian
+    λ += λ0; // move λ from zonal to global coordinates
+    
+    // round to reasonable precision
+    const lat = Number(φ*180/Math.PI); // nm precision (1nm = 10^-11°)
+    const lon = Number(λ * 180 / Math.PI); // (strictly lat rounding should be φ⋅cosφ!)
+
+
+    return [lat, lon];
+}
 
 /* *******************************************
     example messages from autoronto rosbag
@@ -145,3 +388,20 @@ Sample message for path planner
       }
     },
 ******************************************* */
+
+
+//function used to get the distance between car and object 
+function distance(object,car) {
+
+    var dist_delta = [0];
+
+    //this is only true when an object has been detected.
+    if (object[1] != 0) {
+        for (i = 0; i<= car.length-1;i++ ){
+            dist_delta[i] = object[i] - car[i];
+       }
+
+    }
+    
+    return dist_delta;
+}
--- rosbridge-xviz-connector/xviz-server.js
+++ rosbridge-xviz-connector/xviz-server.js
@@ -3,6 +3,13 @@ const process = require('process');
 
 const {XVIZMetadataBuilder, XVIZBuilder, encodeBinaryXVIZ} = require("@xviz/builder");
 
+// Variables used in the ProjectedPath function
+var memory = 0, index = 0, count= 0, Prev_Odom=[],T0=[], duration = [];
+var test =[0];
+var Mod_Path = []; // This is the modified path to check that only numbers were obtained from the path planner 
+var j = 0; // index used to keep track of the actual number of points in the planned path which are non zero value 
+
+
 const xvizMetaBuider = new XVIZMetadataBuilder();
 // we only have one stream for pose(location) for now
 
@@ -12,7 +19,18 @@ xvizMetaBuider.stream('/vehicle_pose')
 //what we will use to make plot the desired path of the car 
 xvizMetaBuider.stream('/vehicle/trajectory')
 	.category('primitive')
+    .type('polygon');
+
+
+// adding one more to show a box to represent the object seen by the car 
+xvizMetaBuider.stream('/Obstcale/position')
+	.category('primitive')
     .type('polyline');
+
+
+
+
+
 xvizMetaBuider.stream('/camera/image_01').category("primitive").type("image");
 const _metadata = xvizMetaBuider.getMetadata();
 console.log("XVIZ server meta-data: ", JSON.stringify(_metadata));
@@ -40,83 +58,113 @@ function connectionId() {
 
 // add a new location message 
 
-function addLocationToFrame(frameNum, lat, lng, alt, time) {
+function addLocationToFrame(frameNum, car_info, obs_info, CarPath, time) {
 
     let frame = _frameCache.get(frameNum);
     let lastframe = _frameCache.get(frameNum-1);
     let heading = 0;
+    
+    
+    
     if (lastframe) {
         // calculate heading based on current and previous location
         // ref: http://www.movable-type.co.uk/scripts/latlong.html
         let λ1 = lastframe.pose.longitude * 3.1415926 / 180;
-        let λ2 = lng * 3.1415926 / 180;
+        let λ2 = car_info[1]* 3.1415926 / 180;
         let φ1 = lastframe.pose.latitude * 3.1415926 / 180;
-        let φ2 = lat * 3.1415926 / 180;
+        let φ2 = car_info[0] * 3.1415926 / 180;
         let y = Math.sin(λ2-λ1) * Math.cos(φ2);
         let x = Math.cos(φ1)*Math.sin(φ2) - Math.sin(φ1)*Math.cos(φ2)*Math.cos(λ2-λ1);
-        heading = Math.atan2(y, x);
+        heading = Math.atan2(y, x);    
     }
+
+    //this will set the vehicle log, lat, alt for it to be displayed in the map
     if (frame) {
+
+        //car pose info 
         frame.pose = {
-            latitude: lat,
-            longitude: lng,
-            altitude: alt,
+            latitude: car_info[0],
+            longitude: car_info[1],
+            altitude: car_info[2],
             timestamp: time,
-            heading: heading
+            heading: 5/3*Math.PI+car_info[3]
+        };
+
+        //object pose info 
+        frame.Objectpose = {
+            latitude: obs_info[0],
+            longitude: obs_info[1],
+            altitude: obs_info[2],
+            timestamp: time,
+            detected: obs_info[3]
+        };
+
+       //car path information:
+        frame.ProjectedPosition = {
+            point : CarPath
         };
     } else {
         _frameCache.set(frameNum, {
-  	        pose: {
-                latitude: lat,
-                longitude: lng,
-                altitude: alt,
+            pose: {
+                latitude: car_info[0],
+                longitude: car_info[1],
+                altitude: car_info[2],
                 timestamp: time,
-                heading: heading
+                heading: 5/3*Math.PI+car_info[3]
+            },
+
+            Objectposition: {
+                x: obs_info[0],
+                y: obs_info[1],
+                z: obs_info[2],
+                timestamp: time,
+                detected: obs_info[3]
+            },
+            
+            ProjectedPosition: {
+                point : CarPath  
+            
             }
-        });
-
-    }
-
-    console.log("new pose (frame, time, lat, lng, heading): ", frameNum, time, lat, lng, heading)
-}
-
-
-function addCarPathToFrame(frameNum,Vertex){
-
-    let frame =  _frameCache.get(frameNum);
-
-    if (frame) {
-        frame.pathplan = {
-            polygon: Vertex
-        };
-    } else {
-        _frameCache.set(frameNum, {
-            pathplan: {
-                polygon: Vertex
-            }
+       
         });
     }
 
-    console.log("new path Vector, frame, position  ", frameNum);
-
-}
+} 
+ 
 
 function tryServeFrame(frameNum){
     let frame = _frameCache.get(frameNum);
     // for now we only have location so as long as location data is ready, mark the frame ready
     //console.log("try serve ", frameNum,frame);
+
     if (frame && frame.pose /*&& frame.pathplan*/) {
         // frame is ready, serve it to all live connections
         //console.log("serving", frameNum);
+        
         //this line serves the meta data used for the pose of the car
         let xvizBuilder = new XVIZBuilder({metadata: _metadata});
         xvizBuilder.pose('/vehicle_pose').timestamp(frame.pose.timestamp)
             .mapOrigin(frame.pose.longitude, frame.pose.latitude, frame.pose.altitude)
-            .position(0,0,0).orientation(0,0,1.57-frame.pose.heading);
-        xvizBuilder.primitive('/vehicle/trajectory').polyline([[2*Math.cos(1.57-frame.pose.heading), 2*Math.sin(1.57-frame.pose.heading), 0], [10*Math.cos(1.57-frame.pose.heading), 10*Math.sin(1.57-frame.pose.heading), 0]]).style({
+            .position(0,0,0).orientation(0,0,frame.pose.heading);
+      //  console.log(frame.ProjectedPosition.Path);
+
+        xvizBuilder.primitive('/vehicle/trajectory').polyline(frame.ProjectedPosition.point).style({
             stroke_color: '#009500',//rgba(0, 150, 0, 0.3)
-            stroke_width: 1.5
-        });
+            stroke_width: 1.5 });
+
+
+
+        //this is for displaying the positon of the obsatcle in space
+
+        if (frame.Objectposition.detected==1){                   
+            xvizBuilder.primitive('/Obstcale/position').polygon([[frame.Objectposition.x,frame.Objectposition.y,0],[frame.Objectposition.x+1,frame.Objectposition.y,0],[frame.Objectposition.x+1,frame.Objectposition.y+1,1],[frame.Objectposition.x,frame.Objectposition.y+1,1]]).style({
+                stroke_color: '#ff0000',//rgba(150, 0, 0, 0.3)
+                stroke_width: 1.5
+            });
+        }
+        
+        
+
         //xvizBuilder.primitive('/camera/image_01').image(_mockImage, "jpg").dimensions(500, 231);
 	    const xvizFrame = JSON.stringify(xvizBuilder.getFrame());
         //console.log(`frame ${frameNum} is ready. `, xvizFrame);
@@ -220,14 +268,91 @@ module.exports = {
         _wss.close();
     },
 
-    updateLocation: function(frameNum, lat, lng, alt, time) {
-        addLocationToFrame(frameNum, lat, lng, alt, time);
+    updateLocation: function(frameNum, car_info, obs_info, Car_Odom,Car_Path, time) {
+
+
+
+
+       test = ProjectedPath(Car_Odom,Car_Path,time)
+        
+      console.log(test);
+
+      Planned_Path = [0,0,0]
+
+
+        addLocationToFrame(frameNum, car_info, obs_info, Planned_Path, time);
         tryServeFrame(frameNum);
-    },
 
-    updateCarPath: function(frameNum,Vertex) {
-        addCarPathToFrame(frameNum,Vertex);
-        //tryServeFrame(frameNum);
+
+
     }
-
+    
 };
+
+function ProjectedPath(Car_Odom,Car_Path,time){
+   
+    /*This is where we define the number of points which will make up the size of the path seen by XViz. If you can to reduce aliasing of the line increase the number of rows. 
+    For this example the predicted path will have 3 points: 
+    P1 = [0,0,0] since the x,y,z value are relative to the position of the car setting them to zero sticks the predicted path at the origin of the car. 
+    P2 = [x2,y2,z2]
+    P3 = [X3,y3,z3] 
+
+    Both P2 and P3 are adjustable depending on where we want to index the path planner Ros topic. For now P3 is index 10 which is 2m away from the car. 
+
+*/
+
+    var  i, Lg ;
+    var Look_Ahead_dist = 5; //this is the distance for 1m  
+    var points = 8; // change this if you want the car to have more or less points. The more points you add the further the car will look in the future. 
+    var Path = [];
+    
+    Lg = Car_Path.length; // overall length of predicted path. 
+    //Lg = 103; //Only the first 103 items have the path info (found experimentally)
+ 
+
+
+    //This loop is meant to only extract points which are not NaN. 
+    for (j = 0; j <= Lg; j++){
+        if (Number.isNaN(Car_Path(j) == false)){
+            Mod_Path[j] = Car_Path[j];
+        }
+    }
+    
+
+
+    /*
+    if(T0==0){
+
+        T0 = time;
+    }
+        
+    duration = time -T0;
+        //check if the elpased time is greater than one second
+    if (duration - memory >1){
+        memory = duration;
+   
+        //only increase the projected path when the car is moving
+        if (Car_Odom[0]>0){
+            count = count +1; // this counter is used to increment the look ahead distance once a delta of 1s occurs. 
+               
+        }
+        }
+
+    if (index <= Lg){
+        
+        //this generates a path as long as the number of points I wish to display
+        for ( i = 1; i <= points; i ++ ){
+          // setting this at 0,0,0 will always make the path start at the base of the car 
+          Path[0] = [0,0,0];           
+         
+          index = count+i*Look_Ahead_dist;
+          Path[i] = Car_Path[index];
+        }
+        
+        
+
+    }
+        
+        */
+    return Path;
+}
